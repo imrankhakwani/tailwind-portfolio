d6bd29f898f6929dbe18c61db21f12faf53ead8d
imrankhakwani <30284761+imrankhakwani@users.noreply.github.com>
2023-01-23 13:57:00 +0500

    Final code

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+const initApp = () => {
+    const hamburgerButton = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+        hamburgerButton.classList.toggle('toggle-btn');
+    }
+
+    hamburgerButton.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+}
+
+document.addEventListener('DOMContentLoaded', initApp);
